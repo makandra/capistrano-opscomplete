54cfad09651ae2a5696411164b206ec872a2b8f9
Henning Koch <henning.koch@makandra.de>
2020-04-16 14:05:15 +0200

    rake is a runtime dependency

--- lib/capistrano/opscomplete.rb
+++ lib/capistrano/opscomplete.rb
@@ -1,5 +1,8 @@
 require 'capistrano/opscomplete/version'
 
+# Need to require rake before we can use the Rake DSL (task do ..., namespace do, ...)
+require 'rake'
+
 load File.expand_path('opscomplete/ruby.rake', __dir__)
 load File.expand_path('opscomplete/supervisor.rake', __dir__)
 load File.expand_path('opscomplete/deploy.rake', __dir__)
